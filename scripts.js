2b1c20c8bfb554acdb38cc9e8d66e0c918d79629
Renushri Rawat <81512748+renushrii@users.noreply.github.com>
2024-08-02 12:10:09 +0530

    changes

---
+++ scripts.js
@@ -0,0 +1,7 @@
+document.getElementById('blogBox').addEventListener('click', function() {
+    alert('Welcome to the Blog section!');
+});
+
+document.getElementById('poetBox').addEventListener('click', function() {
+    alert('Welcome to the Poet section!');
+});
